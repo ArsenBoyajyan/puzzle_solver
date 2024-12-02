c8b25948ecbb34687f34dd0479d92fdee54a5fb0
Arsen <arsboyajyan@gmail.com>
2024-12-03 02:27:20 +0800

    comments deleted

--- main.cpp
+++ main.cpp
@@ -1,43 +1,53 @@
 #include "sokoban.hpp"
+#include <fstream>
 
 int main() {
     std::vector<std::string> grid;
-    read_map(grid); 
+    read_map(grid);
 
     std::string result = solve(grid);
 
-    position p;
-    p.grid = grid;
+    // position p;
+    // p.grid = grid;
+
+    // std::ofstream log("log.txt");
+    // if (result == "No solution!") {
+    //     log << "No solution!" << std::endl;
+    //     std::cout<<"No solution!" << std::endl;
+    //     return 0;
+    // }
+
+    // log << "Solved!!!:" << std::endl;
 
     // for (int i = 0; i < (int)result.size(); ++i) {
     //     switch(result[i]) {
     //         case 'U':
     //             get_up(p, p);
     //             for (int j =0; j < (int)grid.size(); ++j) {
-    //                 std::cout << p.grid[j] << std::endl;
+    //                 log << p.grid[j] << std::endl;
     //             }
-    //             std::cout << "UP\n";
+    //             log << "--------------------\n";
     //             break;
     //         case 'D':
     //             get_down(p, p);
     //             for (int j =0; j < (int)grid.size(); ++j) {
-    //                 std::cout << p.grid[j] << std::endl;
+    //                 log << p.grid[j] << std::endl;
     //             }
-    //             std::cout << "DOWN\n";
+    //             log << "--------------------\n";
     //             break;
     //         case 'L':
     //             get_left(p, p);
     //             for (int j =0; j < (int)grid.size(); ++j) {
-    //                 std::cout << p.grid[j] << std::endl;
+    //                 log << p.grid[j] << std::endl;
     //             }
-    //             std::cout << "LEFT\n";
+    //             log << "--------------------\n";
     //             break;
     //         case 'R':
     //             get_right(p, p);
     //             for (int j =0; j < (int)grid.size(); ++j) {
-    //                 std::cout << p.grid[j] << std::endl;
+    //                 log << p.grid[j] << std::endl;
     //             }
-    //             std::cout << "RIGHT\n";
+    //             log << "--------------------\n";
     //             break;
     //         default:
     //             break;
@@ -45,5 +55,7 @@ int main() {
     // }
 
     std::cout << result << std::endl;
+
+    // log.close();
     return 0;
 }
